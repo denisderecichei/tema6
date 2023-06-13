53cc4d0b8a2c66bf885751935ada730021857f9c
Denis Derecichei <denis.derecichei@gmail.com>
2023-06-13 22:57:31 +0300

    Completare si finalizare tema 5

--- src/main/java/org/tema5/Aprofundare.java
+++ src/main/java/org/tema5/Aprofundare.java
@@ -1,13 +1,60 @@
 package org.tema5;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Aprofundare {
+    private static Random random = new Random();
 
     public static void main(String[] args) {
 //        ex5(12, "curs");
         int[] divizori = divide(100, 10);
         System.out.println("-----------");
         printArray(divizori);
+        ex1();
+        ex2(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
     }
+
+    public static void ex1() {
+        int i = 0;
+        for (i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+        i = 0;
+        while (i < 100) {
+            System.out.println(i);
+            i++;
+        }
+        i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 100);
+    }
+
+    public static void ex2(int[] array) {
+        for (int current : array) {
+            System.out.print(current);
+            System.out.print(" ");
+        }
+    }
+
+    public static void ex3(String[] array) {
+        for (int i = 0; i < array.length; i += 2) {
+            System.out.println(array[i]);
+        }
+    }
+
+    public static void ex4(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) {
+                System.out.print(array);
+            } else {
+                System.out.println(array);
+            }
+        }
+    }
+
 
     public static void ex5(int size, String toBeAdded) {
         if (toBeAdded.length() >= size) {
@@ -29,6 +76,28 @@ public class Aprofundare {
         System.out.println(stringBuffer);
     }
 
+    //guess the number
+    public static void ex6() {
+        //generare random intre 1 si 100 (initial 0-99 apoi +1)
+        int randomNumber = random.nextInt(100) + 1;
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Enter your guess (1-100): ");
+            int userInput = scanner.nextInt();
+
+            if (userInput < randomNumber) {
+                System.out.println("Sorry, you're too low. Try again.");
+            } else if (userInput > randomNumber) {
+                System.out.println("Sorry, you're too high. Try again.");
+            } else {
+                System.out.println("You guessed it! Congratulations!");
+                break;
+            }
+        }
+    }
+
+    //acesta este ex7
     public static int[] divide(int n, int k) {
 //        int counter = 0;
         int sizeArray = n / k;
@@ -45,7 +114,7 @@ public class Aprofundare {
     }
 
     public static void printArray(int[] array) {
-        for (int current: array) {
+        for (int current : array) {
             System.out.print(current);
             System.out.print(" ");
             System.out.println("ADAUGAT DIN GIT");
